470d5108c9d1cad21fc8fb2d4a32d55f7da71e85
slk500 <slawomir.grochowski@gmail.com>
2017-11-19 16:53:47 -0500

    works

--- public/index.php
+++ public/index.php
@@ -1,7 +1,28 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: slk
- * Date: 11/19/17
- * Time: 6:21 AM
- */
+<?php declare(strict_types=1);
+
+require_once '../vendor/autoload.php';
+
+use Grochowski\StepZone\DTO\IssResponse;
+use GuzzleHttp\Client as GuzzleClient;
+
+$guzzle = new GuzzleClient();
+
+$response = $guzzle->get('https://api.wheretheiss.at/v1/satellites/25544');
+
+$responseBodyJson = $response->getBody()->getContents();
+$responseBodyArray = json_decode($responseBodyJson, true);
+
+$issResponse = new IssResponse($responseBodyArray);
+
+$response = $guzzle->get('https://maps.googleapis.com/maps/api/geocode/json', [
+    'query' => [
+        'latlng' => $issResponse->getLatitude() .','. $issResponse->getLongitude()
+    ]]);
+
+$responseBodyJson = $response->getBody()->getContents();
+$responseBodyArray = json_decode($responseBodyJson, true);
+
+var_dump($responseBodyArray);
+
+var_dump($responseBodyArray['results']['0']['formatted_address']);
+
